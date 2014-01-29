9b7b9c68fc3392afab5b002244cb044af898b125
Christophe VG <contact@christophe.vg>
2014-01-29 11:06:07 +0100

    added Graphviz-based visualisation of ASTs

--- parser/dump-ast.py
+++ parser/dump-ast.py
@@ -7,10 +7,13 @@ import sys
 
 from foo_lang import foo_lang
 from antlr3 import RecognitionException
+import antlr3.extras
 
 if len(sys.argv) < 2:
   print "ERROR: please provide a foo-lang source file"
   sys.exit(2)
+
+style = sys.argv[2] if len(sys.argv) > 2 else "dump"
 
 def print_indented(tree, indent):
   print('{0}{1}'.format("  "*indent, tree.text))
@@ -20,7 +23,13 @@ def print_indented(tree, indent):
 try:
   input = open(sys.argv[1]).read()
   lines = input.split("\n")
-  print_indented(foo_lang.parse(input).tree, 0)
+  tree = foo_lang.parse(input).tree
+
+  if style == "dot":
+    print antlr3.extras.toDOT(tree)
+  else:
+    print_indented(tree, 0);
+
 except RecognitionException as e:
   print "Exception:", e, ":"
   print "  index  :", e.index
