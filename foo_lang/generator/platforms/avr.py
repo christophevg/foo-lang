f594d83dfb21409bc345a990073be86cd4f1a9dd
Christophe VG <contact@christophe.vg>
2014-03-13 13:31:10 +0100

    added timestamp + exception handler for AVR types

--- foo_lang/generator/platforms/avr.py
+++ foo_lang/generator/platforms/avr.py
@@ -7,10 +7,15 @@ from codecanvas.platform import Platform
 
 class AVR(Platform):
   def type(self, type):
-    return {
-      "ByteType"    : "uint8_t",
-      "BooleanType" : "uint8_t",
-      "IntegerType" : "uint16_t",
-      "FloatType"   : "float",
-      "LongType"    : "uint16_t"
-    }[str(type)]
+    try:
+      return {
+        "ByteType"    : "uint8_t",
+        "BooleanType" : "uint8_t",
+        "IntegerType" : "uint16_t",
+        "FloatType"   : "float",
+        "LongType"    : "uint16_t",
+
+        "timestamp"   : "time_t"
+      }[str(type)]
+    except:
+      return str(type)
